eda5ccdaf6dce9c07007be69c2135d46a5266903
khalamacki <gpatsfan88@msn.com>
2014-11-12 20:34:05 -0500

    Updated Inventory

--- src/com/example/longdungeon/InventoryActivity.java
+++ src/com/example/longdungeon/InventoryActivity.java
@@ -9,25 +9,78 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class InventoryActivity extends ActionBarActivity {
-
+private LinearLayout itemLayout, itemSubLayout;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inventory);
 
-		Button btnCong = (Button) this.findViewById(R.id.buttonCong);
-		btnCong.setOnClickListener(new OnClickListener() {
+		String[] items = {"Weapon", "Helmet", "Shield", "Clothes"};
+		
+		Button[] btnItemArray = new Button[items.length];
+		for(int i=0; i<items.length; i++){
+			btnItemArray[i] = new Button(this);
+			btnItemArray[i].setText(items[i]);
+			btnItemArray[i].setOnClickListener(new OnClickListener(){
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intentCongrate = new Intent(InventoryActivity.this,
-						CongrateActivity.class);
-				startActivity(intentCongrate);
-			}
-		});
+				@Override
+				public void onClick(View v) {
+					itemCatClicked(v);
+					
+				}});
+		}
+		
+		itemLayout = (LinearLayout) this.findViewById(R.id.itemLayout);
+		if (itemLayout.getChildCount() > 0) {
+			itemLayout.removeAllViews();
+		}
+		for (int i = 0; i < items.length; ++i) {
+			itemLayout.addView(btnItemArray[i]);
+		}
+		
+//		Button btnCong = (Button) this.findViewById(R.id.buttonCong);
+//		btnCong.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				Intent intentCongrate = new Intent(InventoryActivity.this,
+//						CongrateActivity.class);
+//				startActivity(intentCongrate);
+//			}
+//		});
+	}
+	
+	public void itemCatClicked(View v){
+		String[] items = {"Sword", "Axe"};
+		
+		Button[] btnItemSubArray = new Button[items.length];
+		for(int i=0; i<items.length; i++){
+			btnItemSubArray[i] = new Button(this);
+			btnItemSubArray[i].setText(items[i]);
+			btnItemSubArray[i].setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					itemSubCatClicked(v);
+					
+				}});
+		}
+		
+		itemSubLayout = (LinearLayout) this.findViewById(R.id.itemSubLayout);
+		if (itemSubLayout.getChildCount() > 0) {
+			itemSubLayout.removeAllViews();
+		}
+		for (int i = 0; i < items.length; ++i) {
+			itemSubLayout.addView(btnItemSubArray[i]);
+		}
+	}
+	
+	public void itemSubCatClicked(View v){
+		
 	}
 
 	@Override
